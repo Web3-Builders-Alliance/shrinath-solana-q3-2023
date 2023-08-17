cc85d8113a505bc1c4666d1f2eb7255c12f51361
ShrinathNR <nr.shrinath@gmail.com>
2023-08-17 20:19:01 +0530

    add spl init & spl mint

---
+++ cluster/spl_init.ts
@@ -0,0 +1,26 @@
+import { Keypair, Connection, Commitment } from "@solana/web3.js"; 
+import { createMint } from "@solana/spl-token";
+import wallet from "../pre-req/wba-wallet.json";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const commitment : Commitment = "confirmed";
+
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+(async()=>{
+    try{
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            6
+        );
+         console.log(`successfully created a mint acc,\n mint address: ${mint}`);
+    }catch(e){
+        console.log(`something went wrong ${e}`)
+    }
+})();
+
+
+// mint address = "BfnBLUYWBjTwDDvkeLXMJ7VotmULsoHr88WtyZJSikhZ"
---
+++ cluster/spl_mint.ts
@@ -0,0 +1,29 @@
+import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import wallet from "../pre-req/wba-wallet.json"
+
+// Import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+const token_decimals = 1_000_000;
+
+
+const mint = new PublicKey("BfnBLUYWBjTwDDvkeLXMJ7VotmULsoHr88WtyZJSikhZ");
+
+(async () => {
+    try {
+        // Create an ATA
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair,mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+
+        // Mint to ATA
+        const mintTx = await mintTo(connection,keypair,mint,ata.address,keypair.publicKey,100*token_decimals);
+        console.log(`Your mint txid: ${mintTx}`);
+    } catch(error) {
+        console.log(`Oops, something went wrong: ${error}`)
+    }
+})()
