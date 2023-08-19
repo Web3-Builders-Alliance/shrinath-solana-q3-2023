77d5fc06cf8875a142febe5154442ce05c8cacbc
ShrinathNR <nr.shrinath@gmail.com>
2023-08-19 17:11:32 +0530

    add vault deposit spl

--- pre-req/keygen.ts
+++ pre-req/keygen.ts
@@ -1,6 +1,11 @@
 import { Keypair } from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+
+const dev_kp = Keypair.fromSecretKey(new Uint8Array(wallet));
+// console.log(dev_kp);
+// console.log(dev_kp.publicKey.toBase58());
 
 //generate keypair
-let kp = Keypair.generate();
-console.log(`genrated a new solana wallet : ${kp.publicKey.toBase58()}`);
-console.log(kp);
+// let kp = Keypair.generate();
+// console.log(`genrated a new solana wallet : ${kp.publicKey.toBase58()}`);
+// console.log(kp);
