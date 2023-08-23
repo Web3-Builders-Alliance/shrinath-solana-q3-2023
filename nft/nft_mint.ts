0780744b18c77fb3198d63859f068d4dcd0b0a2c
ShrinathNR <nr.shrinath@gmail.com>
2023-08-23 15:52:12 +0530

    add nft mint

---
+++ nft/nft_mint.ts
@@ -0,0 +1,35 @@
+// metadata URI = https://arweave.net/giuyRgVvsd3CdMfbYEpxHFlfoeiokAhwLZJDKu_QwAc
+
+import { Keypair,Connection, Commitment } from "@solana/web3.js";
+import { Metaplex, keypairIdentity, bundlrStorage} from "@metaplex-foundation/js";
+import wallet from "../pre-req/wba-wallet.json";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+const commitment:Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(keypair))
+    .use(bundlrStorage({
+        address:'https://devnet.bundlr.network',
+        providerUrl:'https://api.devnet.solana.com',
+        timeout:60000,
+    }));
+
+(async()=>{
+    try {
+        const {nft} =await metaplex
+        .nfts()
+        .create({
+            uri:"https://arweave.net/giuyRgVvsd3CdMfbYEpxHFlfoeiokAhwLZJDKu_QwAc",
+            name:"resiquents generug",
+            sellerFeeBasisPoints:0,
+        });
+        
+        console.log(`Success! Check out your NFT here:\nhttps://explorer.solana.com/address/${nft.address.toBase58()}?cluster=devnet`);
+
+    } catch (error) {
+        console.log(`something went wrong : ${error}`)
+    }
+})();
