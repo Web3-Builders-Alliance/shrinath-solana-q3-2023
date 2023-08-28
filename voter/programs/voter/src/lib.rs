3b0e149310d2b654e14133a000024e913ee87814
ShrinathNR <nr.shrinath@gmail.com>
2023-08-28 20:14:36 +0530

    forgot to save lib.rs

--- voter/programs/voter/src/lib.rs
+++ voter/programs/voter/src/lib.rs
@@ -6,18 +6,18 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 mod vote_anchor {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>, _hash: Vec<u8>) -> Result<()> {
-        ctx.accounts.vote_account.votes = 0i64;
-        ctx.accounts.vote_account.bump = *ctx.bumps.get("vote_account").unwrap();
+        ctx.accounts.vote.votes = 0i64;
+        ctx.accounts.vote.bump = *ctx.bumps.get("vote").unwrap();
         Ok(())
     }
 
     pub fn upvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
-        ctx.accounts.vote_account.votes+=1;
+        ctx.accounts.vote.votes+=1;
         Ok(())
     }
 
     pub fn downvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
-        ctx.accounts.vote_account.votes-=1;
+        ctx.accounts.vote.votes-=1;
         Ok(())
     }
 }
@@ -28,7 +28,7 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
     #[account(init, seeds = [b"vote", _hash.as_slice().as_ref()], bump, payer = owner, space = 8 + 9 + 32)]
-    pub vote_account: Account<'info, Vote>,
+    pub vote: Account<'info, Vote>,
     pub system_program: Program<'info, System>,
 }
 
@@ -37,8 +37,8 @@ pub struct Initialize<'info> {
 pub struct VoteInteraction<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
-    #[account(mut, seeds = [b"vote", _hash.as_slice().as_ref()], bump = vote_account.bump)]
-    pub vote_account: Account<'info, Vote>
+    #[account(mut, seeds = [b"vote", _hash.as_slice().as_ref()], bump = vote.bump)]
+    pub vote: Account<'info, Vote>
 }
 
 #[account]
