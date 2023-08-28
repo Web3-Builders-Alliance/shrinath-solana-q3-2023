ae57eb83097ab1bc50e81da489c06dfc358b9f02
ShrinathNR <nr.shrinath@gmail.com>
2023-08-28 20:13:30 +0530

    edit voter

--- voter/programs/voter/src/lib.rs
+++ voter/programs/voter/src/lib.rs
@@ -3,56 +3,46 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod voter {
+mod vote_anchor {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, _hash : Vec<u8>) -> ProgramResult {
-        ctx.accounts.vote.votes = 0;
-        ctx.accounts.vote.bump= *ctx.accounts.bump.get("vote").unwrap();
+    pub fn initialize(ctx: Context<Initialize>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote_account.votes = 0i64;
+        ctx.accounts.vote_account.bump = *ctx.bumps.get("vote_account").unwrap();
         Ok(())
     }
 
-    pub fn upvote(Context<VoteInteraction>)-> Result<()> {
-        ctx.accounts.vote.votes += 1;
-        Ok(())
-    }
-    pub fn downvote(Context<VoteInteraction>)-> Result<()> {
-        ctx.accounts.vote.votes -= 1;
+    pub fn upvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote_account.votes+=1;
         Ok(())
     }
 
+    pub fn downvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote_account.votes-=1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 #[instruction(_hash: Vec<u8>)]
 pub struct Initialize<'info> {
     #[account(mut)]
-    signer : Signer<'info>
-    #[account(
-        init,
-        payer : signer,
-        space : 8+8+1,
-        seeds : [b"vote", _hash.as_slice().as_ref()],  
-        bump,    
-    )]
-    vote: Account<'info,Vote>,
-    system_program : Program<'info, System>,
+    pub owner: Signer<'info>,
+    #[account(init, seeds = [b"vote", _hash.as_slice().as_ref()], bump, payer = owner, space = 8 + 9 + 32)]
+    pub vote_account: Account<'info, Vote>,
+    pub system_program: Program<'info, System>,
 }
+
 #[derive(Accounts)]
-#[instruction(_hash : Vec<u8>)]
+#[instruction(_hash: Vec<u8>)]
 pub struct VoteInteraction<'info> {
     #[account(mut)]
-    signer : Signer<'info>
-    #[account(
-        mut,
-        payer : signer,
-        seeds : [b"vote", _hash.as_slice().as_ref()],  
-        bump = vote.bump,    
-    )]
-    vote: Account<'info,Vote>,
+    pub owner: Signer<'info>,
+    #[account(mut, seeds = [b"vote", _hash.as_slice().as_ref()], bump = vote_account.bump)]
+    pub vote_account: Account<'info, Vote>
 }
 
 #[account]
 pub struct Vote {
-    votes : i64,
-    bump : u8,
+    votes: i64,
+    bump: u8
 }
